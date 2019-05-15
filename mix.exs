9148d8f2f31e19fdc29063810c5ea72bf7b2ef2d
Jean-Sébastien Pédron <jean-sebastien.pedron@dumbbell.fr>
2019-05-15 15:37:03 +0200

    mix.exs: Add application description
    ... so it matches the one in `ebin/stdout_formatter.app.src`.

--- mix.exs
+++ mix.exs
@@ -4,6 +4,7 @@ defmodule StdoutFormatter.MixProject do
   def project do
     [
       app: :stdout_formatter,
+      description: "Tools to format paragraphs, lists and tables as plain text",
       version: "0.2.3",
       language: :erlang,
       deps: []
